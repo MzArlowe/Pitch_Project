038d8beae81570dd8dbcb3f15cab66e640bc649b
mfronterhouse <mallory.fronterhouse@mykenzie.snhu.edu>
2023-01-11 22:40:46 -0500

    user model updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,22 +2,30 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");//Is reference to database file, folder structure may change
 const UserModel = db.define("user", {
     firstName: {
-        type: DataTypes.STRING(1000),
+        type: DataTypes.STRING(30), // values can change
         allowNull: false
     },
     lastName: {
-        type: DataTypes.STRING(1000),
+        type: DataTypes.STRING(30), // values can change
         allowNull: false
     },
     email: {
-        type: DataTypes.STRING(1000),
+        type: DataTypes.STRING(50), // values can change
         allowNull: false,
+        unique: true,
+        required : true // will this be needed if username is also an option?
+    },
+    username: {
+        type: DataTypes.STRING(30), // can be changed. added in for more flexibility when signing in
+        allowNull : false,
         unique: true
     },
     password: {
-        type: DataTypes.STRING(1000),
+        type: DataTypes.STRING(20), // values can change
         allowNull: false,
-        unique : true
+        unique : true,
+        minlength : 6, // value can change
+        required : true
     },
     isFounder: {
         type: DataTypes.BOOLEAN,
