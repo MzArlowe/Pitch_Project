2e0ebd41adbce62e153cad523110200b6ff49496
mfronterhouse <mallory.fronterhouse@mykenzie.snhu.edu>
2023-01-25 21:56:20 -0500

    user file refactored

--- src/db.js
+++ src/db.js
@@ -7,8 +7,8 @@ const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 })
-//  const db = new Sequelize(process.env.DB_CONNECTION_STRING);
-// Database connection string
-// <db type>://<username>:<password>@ip:port/<db name>
-// postgres://postgres:dbLocal@localhost:5432/pie
+//const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+//Database connection string
+{/* <db type>://<username>:<password>@ip:port/<db name>
+postgres://postgres:dbLocal@localhost:5432/pie */}
 module.exports = db;
