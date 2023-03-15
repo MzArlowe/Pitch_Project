05bdfdaa34f953f7bb403f7645d5ef9c3f29c3a6
Ben Marlowe <bhm727@yahoo.com>
2023-03-14 21:37:50 -0400

    changes to App.js test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Like a Pro
         </a>
       </header>
     </div>
